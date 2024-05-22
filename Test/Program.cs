fedd18d628651c34236adc63a52ac5fe94f70144
4 <4@DESKTOP-SRIGVJA>
2024-05-22 00:59:12 -0300

    .

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,7 +7,6 @@ namespace Test {
             Libro miLibro = new Libro("titulo", "autorx", 4444, "444-44-444-4444-4", "4444444444", 4444);
 
             Mapa miMapa = new Mapa("titulo mapa", "autorx mapa", 4444, "444-44-444-4444-4 mapa", "4444mapa", 44, 44);
-
         }
     }
 }
